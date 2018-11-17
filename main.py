a4f583b9821db99876b6f7474db0c9d088fe67e0
Felix Schoeler <fela98@live.no>
2018-11-17 15:44:10 +0100

    Refactored and added evaluation

--- main.py
+++ main.py
@@ -3,18 +3,36 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation
 import numpy as np
 
-mnist = mnistdata.read_data_sets("data", one_hot=True, reshape=False)
 
+def main():
+    mnist = mnistdata.read_data_sets("data", one_hot=True, reshape=False)
 
+    model = simple_model()
 
-model = Sequential()
-model.add(Dense(32, input_dim=784))
-model.add(Dense(10, activation='softmax'))
-model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+    print("\n|---- TRAINING ----|")
 
-batchsize = 100
+    batch_size=100
 
-flattened_images = np.reshape(mnist.train.images, (mnist.train.images.shape[0], 28*28))
+    model.fit(flatten_images(mnist.train.images), mnist.train.labels, epochs=1, batch_size=batch_size)
 
-model.fit(flattened_images, mnist.train.labels, epochs=1, batch_size=100)
+    print("\n|---- EVALUATING ----|")
 
+    score = model.evaluate(flatten_images(mnist.test.images), mnist.test.labels, batch_size=batch_size)
+
+    print(model.metrics_names[1] + ": ", "%.2f" % (score[1]*100), "%")
+
+    return
+
+def simple_model():
+    model = Sequential()
+    model.add(Dense(32, input_shape=(784,)))
+    model.add(Dense(10, activation='softmax'))
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    return model
+
+def flatten_images(arr):
+    return np.reshape(arr, (arr.shape[0], arr.shape[1]*arr.shape[2]))
+
+if __name__ == "__main__":
+    main()
